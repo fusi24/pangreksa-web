774ff83eb50cb18ede3a8010c9ad99c09cb14c13
Singgih Yunanto <singgih.yunanto@gmail.com>
2025-07-04 11:51:11 +0700

    initial commit

---
+++ src/main/kotlin/com/fusi24/pangreksa/web/PangreksaWebApplication.kt
@@ -0,0 +1,11 @@
+package com.fusi24.pangreksa.web
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PangreksaWebApplication
+
+fun main(args: Array<String>) {
+	runApplication<PangreksaWebApplication>(*args)
+}
