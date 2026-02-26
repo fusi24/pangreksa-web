d20334e829cd7b1932be92271d399328b5fb0557
iqbalchandraessentials <iqbalchandra96@gmail.com>
2026-02-26 10:09:51 +0700

    "Last Login time zone fixing"

--- src/main/java/com/fusi24/pangreksa/security/LoginSuccessListener.java
+++ src/main/java/com/fusi24/pangreksa/security/LoginSuccessListener.java
@@ -5,29 +5,20 @@ import com.fusi24.pangreksa.web.repo.FwAppUserRepository;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 
 @Component
 public class LoginSuccessListener {
-
     private final FwAppUserRepository userRepository;
-
-
-
     public LoginSuccessListener(FwAppUserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
     @EventListener
     public void handleLoginSuccess(AuthenticationSuccessEvent event) {
-
         String username = event.getAuthentication().getName();
         FwAppUser user = userRepository.findByUsername(username).orElse(null);
-
         if (user != null) {
             user.setLastLogin(
                     ZonedDateTime.now(ZoneId.of("Asia/Jakarta")).toLocalDateTime()
