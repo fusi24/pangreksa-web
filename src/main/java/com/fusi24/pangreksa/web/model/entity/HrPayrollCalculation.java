b6a7885c97e4a73c6299cc57dddeb1cb1c8644fb
Iman Sanjaya <imansanjaya800@gmail.com>
2026-04-14 16:19:01 +0700

    Update Payroll With PPh 21 TER for Enhancement Payroll V.2.1.1

--- src/main/java/com/fusi24/pangreksa/web/model/entity/HrPayrollCalculation.java
+++ src/main/java/com/fusi24/pangreksa/web/model/entity/HrPayrollCalculation.java
@@ -15,6 +15,8 @@ import java.time.LocalDateTime;
 @Builder
 public class HrPayrollCalculation {
 
+    //Iman
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
